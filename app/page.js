3f6674e4a5a8d40f9c29404b6f5a2fa62316025e
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-02-04 22:50:41 +0600

    feat: update dependencies and add typography plugin for Tailwind CSS
    - Added @tailwindcss/typography to dependencies
    - Updated Next.js version to 14.2.35
    - Reorganized package.json dependencies and devDependencies
    - Added new logo.svg file to public directory
    - Added google-io-2023-1.png to public/events directory
    - Updated tailwind.config.js to center container and set max width
    - Enabled typography plugin in Tailwind CSS configuration

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import EventList from "@/components/landing/EventList";
+import Header from "@/components/landing/Header";
 
-export default Home;
+export default function Home() {
+  return (
+    <section className="container">
+      <Header />
+      <EventList />
+    </section>
+  );
+}
