bb40e1dd58c42c4f3a1e42178e15a074cd5a49a7
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-02-07 04:23:05 +0600

    feat: add debounce hook and implement debounced search handling in Search component

--- components/landing/Search.jsx
+++ components/landing/Search.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useDebounce } from "@/hooks/debounce";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 const Search = () => {
@@ -17,13 +18,16 @@ const Search = () => {
 
     replace(`${pathName}?${params.toString()}`);
   };
+
+  const handleSearchDebounced = useDebounce(handleSearch, 500);
+
   return (
     <div>
       <input
         type="text"
         placeholder="Search..."
         className="bg-[#27292F] border border-[#CCCCCC]/20 py-1 px-2 rounded-md"
-        onChange={(e) => handleSearch(e.target.value)}
+        onChange={(e) => handleSearchDebounced(e.target.value)}
         defaultValue={searchParams.get("query")?.toString()}
       />
     </div>
